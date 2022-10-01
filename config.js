9fe14e90b5edd88d3910e81412f09b2161c34ef8
Djpradeep123 <112794230+Djpradeep123@users.noreply.github.com>
2022-10-01 10:23:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,15 +7,15 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
-global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
-global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
+global.Owner = ['918505038661']
+global.OwnerNumber = ['918505038661']
+global.ownertag = ['918505038661']
+global.BotName = "┕━━☽【ᴘʀᴀᴅᴇᴇᴘ】☾━━┙"
+global.packname = "┕━━☽【ᴘʀᴀᴅᴇᴇᴘ】☾━━┙"
+global.author = "By: ┕━━☽【ᴘʀᴀᴅᴇᴇᴘ】☾━━┙"
+global.OwnerName = "┕━━☽【ᴘʀᴀᴅᴇᴇᴘ】☾━━┙"
+global.BotSourceCode = "https://github.com/Djpradeep123/Miku-MD"
+global.SupportGroupLink = "https://chat.whatsapp.com/CJkEfTGUW5p5Evlk3yZd9x"
 global.sessionName = "session"
 
 // Prefix //
@@ -23,11 +23,11 @@ global.sessionName = "session"
 global.prefa = ['-'] 
 
 
-global.location = "West Bengal, India"
+global.location = "jaipur rajasthan, India"
 global.reactmoji = "❤️"
 global.themeemoji = "💖"
 global.vidmenu = { url: 'https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4' }
-global.websitex = "https://github.com/FantoX001"
+global.websitex = "https://github.com/Djpradeep123"
 global.lolhuman = "KaysaS"
 
 
